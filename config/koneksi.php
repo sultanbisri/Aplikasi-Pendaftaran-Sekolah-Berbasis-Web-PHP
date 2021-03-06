38002b2d461f821c155ad70a85d99e2f337ac871
Sultan Bisri <79346157+sultanbisri@users.noreply.github.com>
2021-03-06 22:27:20 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "pendaftaran";
+
+	$koneksi = mysqli_connect($host, $username, $password, $database);
+
+	if($koneksi -> connect_error){
+		echo " Koneksi database gagal ". mysqli_connect_error() ;
+		die;
+	}else{
+		// echo " Koneksi database berhasil";
+	}
+?>
